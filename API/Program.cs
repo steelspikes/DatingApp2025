1a4dc3e8418aa73ce620de1235139290063724cc
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-09-09 07:27:15 -0600

    Parcial01 - Add API and client skeletons (#1)
    * Partial 1, initial configuration
    
    * Parcial01 - Add first entity
    
    * Parcial01 - Install EF Core for SQLite
    
    * Parcial01 - Add first EF migration
    
    * Parcial01 - Setting migration params
    
    * Parcial01 - Create database
    
    * Parcial01 - Add Members Controller
    
    * Parcial01 - Add Angular Skeleton
    
    * Parcial01 - Add Http Client to SPA
    
    * Parcial01 - Allow CORS and display fetched users
    
    * Parcial01 - Add tailwind and DaisyUI for styling
    
    * Parcial01 - Add initial styling
    
    * Parcial01 - Update SSL

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,37 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200",
+        "https://localhost:4200" 
+    ));
 
 app.MapControllers();
 
---
+++ client/src/app/app.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members = signal<any>([]);
+
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(): Promise<Object> {
+    try {
+      return await lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
