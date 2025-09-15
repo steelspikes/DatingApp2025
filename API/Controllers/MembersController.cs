5b94357aa98a33e59c8fef47fd080b258bcfe38a
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-09-15 08:02:01 -0600

    Parcial02 - Add JWT Middleware

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,26 +1,30 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
-{
-    public class MembersController(AppDbContext context) : BaseApiController
-    {
-        [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
-        {
-            var members = await context.Users.ToListAsync();
-            return members;
-        }
+namespace API.Controllers;
 
-        [HttpGet("{id}")] // .../api/members/bob-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
-        {
-            var member = await context.Users.FindAsync(id);
-            if (member == null) return NotFound();
-            return member;
-        }
+[Authorize]
+public class MembersController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+
+        return members;
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+
+        if (member == null) return NotFound();
+
+        return member;
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,42 +1,45 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
-
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"]
+            ?? throw new ArgumentNullException("Cannot get the token key - Program.cs");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
 app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
     .WithOrigins(
         "http://localhost:4200",
-        "https://localhost:4200" 
-    ));
+        "https://localhost:4200"
+    ));
 
 app.MapControllers();
 
