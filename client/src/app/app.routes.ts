ec1c99eaa3affe8d125388b930f13bf32cdfa157
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-10-06 08:15:13 -0600

    Parcial02 - Add Route Guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetail } from '../features/members/member-detail/member-detail';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guard/auth-guard';
 
 export const routes: Routes = [
   { path: "", component: Home },
-  { path: "members", component: MemberList },
+  { path: "members", component: MemberList, canActivate: [authGuard] },
   { path: "members/{id}", component: MemberDetail },
   { path: "lists", component: Lists },
   { path: "messages", component: Messages },
