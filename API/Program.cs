bf7c9e80d3f55f5344567f6dfc14a7fed55e0206
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-10-16 07:34:04 -0600

    Parcial02 - Add data seeding

--- API/Program.cs
+++ API/Program.cs
@@ -44,4 +44,19 @@ app.UseCors(x => x.AllowAnyHeader()
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Migration process failed!");
+}
+
 app.Run();
