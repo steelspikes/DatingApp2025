07f4993f630d361f4148bad5be58af4e33984e24
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-11-27 22:04:27 -0600

    Parcial03 - Add new folder structure for src and tests

---
+++ tests/API.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace API.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
