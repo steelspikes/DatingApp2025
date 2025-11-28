764b31b8972978cb01f1d4570e73a72def355c43
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-11-27 19:08:02 -0600

    Parcial03 - Add members service

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { Observable, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { Observable, tap } from 'rxjs';
 export class AccountService {
   private http = inject(HttpClient);
   currentUser = signal<User | null>(null);
-  baseUrl = "http://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds): Observable<User> {
     return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
