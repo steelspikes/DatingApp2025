32f504e808ad233b8404bdf6ef01b25962f87772
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-02-26 19:13:49 -0600

    Parcial04 - Add member edition

--- client/src/features/member-profile/member-profile.ts
+++ client/src/features/member-profile/member-profile.ts
@@ -1,23 +1,58 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../types/member';
+import { EditableMember, Member } from '../../types/member';
 import { DatePipe } from '@angular/common';
 import { MembersService } from '../../core/services/members-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit {
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('memberProfileEditForm') memberProfileEditForm?: NgForm;
   private route = inject(ActivatedRoute);
+  private toast = inject(ToastService);
   protected member = signal<Member | undefined>(undefined);
   protected membersService = inject(MembersService);
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  };
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data["member"]);
-    })
+    });
+
+    this.editableMember = {
+      displayName: this.member()?.displayName || '',
+      description: this.member()?.description || '',
+      city: this.member()?.city || '',
+      country: this.member()?.country || ''
+    };
+  }
+
+  ngOnDestroy(): void {
+    if (this.membersService.editMode()) {
+      this.membersService.editMode.set(false);
+    }
+  }
+
+  updateProfile() {
+    if (!this.member()) return;
+    const updatedMember = {...this.member(), ...this.editableMember};
+
+    console.group("UPDATE");
+    console.log(updatedMember);
+    console.groupEnd();
+
+    this.toast.success('Profile updated successfully');
+    this.membersService.editMode.set(false);
   }
 }
