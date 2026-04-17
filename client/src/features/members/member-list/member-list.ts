f310da1fc426399a49165c59ac42f5983144b981
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 21:34:08 -0600

    Pacial05 - Add filter modal 2/2

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -8,7 +8,7 @@ import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [MemberCard, Paginator],
+  imports: [MemberCard, Paginator, FilterModal],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
@@ -42,5 +42,14 @@ export class MemberList implements OnInit {
 
   onClose() {
     console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams) {
+    console.log('Modal submitted data: ', data);
+  }
+
+  resetFilters() {
+    this.memberParams = new MemberParams();
+    this.loadMembers();
   }
 }
