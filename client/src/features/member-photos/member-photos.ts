c8690ca37c2a3548adff6ef7bcc3cf9ff1e17cc5
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 21:05:51 -0600

    Parcial05 - Add fix to photo upload

--- client/src/features/member-photos/member-photos.ts
+++ client/src/features/member-photos/member-photos.ts
@@ -43,6 +43,9 @@ export class MemberPhotos implements OnInit {
         this.membersService.editMode.set(false);
         this.loading.set(false);
         this.photos.update(photos => [...photos, photo]);
+        if (!this.membersService.member()?.imageUrl) {
+          this.setMainLocalPhoto(photo);
+        }
       },
       error: error => {
         console.log('Error while uploading the image: ', error);
@@ -54,13 +57,7 @@ export class MemberPhotos implements OnInit {
   setMainPhoto(photo: Photo) {
     this.membersService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-        if (currentUser) currentUser.imageUrl = photo.url;
-        this.accountService.setCurrentUser(currentUser as User);
-        this.membersService.member.update(member => ({
-          ...member,
-          imageUrl: photo.url
-        }) as Member);
+        this.setMainLocalPhoto(photo);
       }
     });
   }
@@ -71,5 +68,15 @@ export class MemberPhotos implements OnInit {
         this.photos.update(photos => photos.filter(p => p.id !== photoId))
       }
     });
+  }
+
+  private setMainLocalPhoto(photo: Photo) {
+    const currentUser = this.accountService.currentUser();
+    if (currentUser) currentUser.imageUrl = photo.url;
+    this.accountService.setCurrentUser(currentUser as User);
+    this.membersService.member.update(member => ({
+      ...member,
+      imageUrl: photo.url
+    }) as Member);
   }
 }
