f5796ca9c01ef5a7400213763f6b4ebecb0ddc93
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 22:50:07 -0600

    Parcial05 - Add Likes List feature

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes-service';
+import { Member } from '../../types/member';
 
 @Component({
   selector: 'app-lists',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists {
+export class Lists implements OnInit {
+  private likesService = inject(LikesService);
+  private members = signal<Member[]>([]);
+  protected predicate = 'liked';
 
+  tabs = [
+    { label: 'Liked', value: 'liked'},
+    { label: 'Liked me', value: 'likedby'},
+    { label: 'Mutual', value: 'mutual'}
+  ];
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string) {
+    if (this.predicate !== predicate) {
+      this.predicate = predicate;
+      this.loadLikes();
+    }
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members.set(members)
+    });
+  }
 }
