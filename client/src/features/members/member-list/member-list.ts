d8352e89b21deb4a6847143169d7843a90d237f0
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 21:35:27 -0600

    Pacial05 - Add age gender filters to modal

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -45,7 +45,8 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams) {
-    console.log('Modal submitted data: ', data);
+    this.memberParams = data;
+    this.loadMembers();
   }
 
   resetFilters() {
