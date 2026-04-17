733be6df84ad0e26aadf58652c680a77d1e024db
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 21:28:52 -0600

    Pacial05 - Add filtering API call from SPA

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { EditableMember, Member, Photo } from '../../types/member';
+import { EditableMember, Member, MemberParams, Photo } from '../../types/member';
 import { Observable, tap } from 'rxjs';
 import { PaginationResult } from '../../types/paginationMetadata';
 
@@ -22,10 +22,13 @@ export class MembersService {
     );
   }
 
-  getMembers(pageNumber = 1, pageSize = 5): Observable<PaginationResult<Member>> {
+  getMembers(memberParams: MemberParams): Observable<PaginationResult<Member>> {
     let params = new HttpParams();
-    params = params.append('pageNumber', pageNumber);
-    params = params.append('pageSize', pageSize);
+    params = params.append('pageNumber', memberParams.pageNumber);
+    params = params.append('pageSize', memberParams.pageSize);
+    params = params.append('minAge', memberParams.minAge);
+    params = params.append('maxAge', memberParams.maxAge);
+    if (memberParams.gender) params = params.append('gender', memberParams.gender);
 
     return this.http.get<PaginationResult<Member>>(this.baseUrl + "members", { params });
   }
--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,30 +1,27 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { Member } from '../../../types/member';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { Member, MemberParams } from '../../../types/member';
 import { MemberCard } from "../member-card/member-card";
 import { PaginationResult } from '../../../types/paginationMetadata';
 import { Paginator } from "../../../shared/paginator/paginator";
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MemberCard, Paginator],
+  imports: [MemberCard, Paginator],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
 export class MemberList implements OnInit {
   private membersService = inject(MembersService);
   protected paginatedMembers = signal<PaginationResult<Member> | null>(null);
-  pageNumber = 1;
-  pageSize = 5;
+  protected memberParams = new MemberParams();
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
   loadMembers() {
-    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+    this.membersService.getMembers(this.memberParams).subscribe({
       next: result => {
         this.paginatedMembers.set(result);
       }
@@ -32,8 +29,8 @@ export class MemberList implements OnInit {
   }
 
   onPageChange(event: { pageNumber: number, pageSize: number }) {
-    this.pageNumber = event.pageNumber;
-    this.pageSize = event.pageSize;
+    this.memberParams.pageNumber = event.pageNumber;
+    this.memberParams.pageSize = event.pageSize;
     this.loadMembers();
   }
 }
