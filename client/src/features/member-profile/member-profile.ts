476545fb23eff92cdb1899ff956603aa4e5e873a
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 22:28:51 -0600

    Parcial05 - Add time ago pipe

--- client/src/features/member-profile/member-profile.ts
+++ client/src/features/member-profile/member-profile.ts
@@ -5,10 +5,11 @@ import { MembersService } from '../../core/services/members-service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastService } from '../../core/services/toast-service';
 import { AccountService } from '../../core/services/account-service';
+import { TimeAgoPipe } from '../../core/pipes/time-ago-pipe';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe, FormsModule],
+  imports: [DatePipe, FormsModule, TimeAgoPipe],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
