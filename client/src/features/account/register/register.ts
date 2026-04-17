fe9f77fac2e7dac258daa029d96e730a27d9c0a1
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-04-16 20:47:32 -0600

    Parcial05 - Add reactive register form

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,27 +1,45 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { RegisterCreds } from '../../../types/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
   private accountService = inject(AccountService);
-  cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+  cancelRegister = output<boolean>();
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
+  }
 
   register(): void {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    });
+    console.group("REGISTER");
+    console.log(this.registerForm.value);
+    console.groupEnd();
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // });
   }
 
   cancel(): void {
