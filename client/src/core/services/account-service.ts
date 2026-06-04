55927edfd3db9ef1146b850a679609db4a0fbeae
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-06-03 18:24:45 -0600

    Parcial06 - IDENTITY - Add SECURITY to token with cookies to client

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -15,34 +15,51 @@ export class AccountService {
   baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds): Observable<User> {
-    return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
+    return this.http.post<User>(this.baseUrl + "account/register", creds, { withCredentials: true }).pipe(
       tap(user => {
         if (user) {
           this.setCurrentUser(user);
+          this.startTokenRefreshInterval();
         }
       })
     );
   }
 
   login(creds: LoginCreds): Observable<User> {
-    return this.http.post<User>(this.baseUrl + "account/login", creds).pipe(
+    return this.http.post<User>(this.baseUrl + "account/login", creds, { withCredentials: true }).pipe(
       tap(user => {
         if (user) {
           this.setCurrentUser(user);
+          this.startTokenRefreshInterval();
         }
       })
     );
   }
 
+  refreshToken() {
+    return this.http.post<User>(this.baseUrl + 'account/token', {}, { withCredentials: true });
+  }
+
+  startTokenRefreshInterval() {
+    setInterval(() => {
+      this.http.post<User>(this.baseUrl + 'account/token', {}, { withCredentials: true }).subscribe({
+        next: user => {
+          this.setCurrentUser(user);
+        },
+        error: () => {
+          this.logout();
+        }
+      })
+    }, 5 * 60 * 1000);
+  }
+
   setCurrentUser(user: User) {
     user.roles = this.getRolesFromToken(user);
-    localStorage.setItem("user", JSON.stringify(user));
     this.currentUser.set(user);
     this.likesService.getLikeIds();
   }
 
   logout() {
-    localStorage.removeItem("user");
     localStorage.removeItem("filters");
     this.likesService.clearLikeIds();
     this.currentUser.set(null);
--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,24 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
-import { Observable, of } from 'rxjs';
-import { LikesService } from './likes-service';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private accountService = inject(AccountService);
-  private likesService = inject(LikesService);
 
-  init(): Observable<null> {
-    const userString = localStorage.getItem("user");
-
-    if (userString) {
-      const user = JSON.parse(userString);
-      this.accountService.currentUser.set(user);
-      this.likesService.getLikeIds();
-    }
-
-    return of(null);
+  init() {
+    return this.accountService.refreshToken().pipe(
+      tap(user => {
+        if (user) {
+          this.accountService.setCurrentUser(user);
+          this.accountService.startTokenRefreshInterval();
+        }
+      })
+    );
   }
 }
