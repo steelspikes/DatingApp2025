d988816a8fbe600532728ea9c4890e06d3cf72ae
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-06-03 18:30:39 -0600

    Parcial06 - SignalR - Add presence hub to client

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -4,6 +4,8 @@ import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes-service';
+import { PresenceService } from './presence-service';
+import { HubConnectionState } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,7 @@ import { LikesService } from './likes-service';
 export class AccountService {
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   currentUser = signal<User | null>(null);
   baseUrl = environment.apiUrl;
 
@@ -57,6 +60,9 @@ export class AccountService {
     user.roles = this.getRolesFromToken(user);
     this.currentUser.set(user);
     this.likesService.getLikeIds();
+    if (this.presenceService.hubConnection?.state !== HubConnectionState.Connected) {
+      this.presenceService.createHubConnection(user);
+    }
   }
 
   logout() {
---
+++ client/src/core/services/presence-service.ts
@@ -0,0 +1,40 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { ToastService } from './toast-service';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { User } from '../../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresenceService {
+  private hubUrl = environment.hubUrl;
+  private toast = inject(ToastService);
+  hubConnection?: HubConnection;
+
+  createHubConnection(user: User) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'presence', {
+        accessTokenFactory: () => user.token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+      this.hubConnection.start()
+        .catch(error => console.log(error));
+
+      this.hubConnection.on('UserOnline', email => {
+        this.toast.success(email + ' has connected!');
+      });
+
+      this.hubConnection.on('UserOffline', email => {
+        this.toast.success(email + ' has disconnected!');
+      });
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection?.state === HubConnectionState.Connected) {
+      this.hubConnection.stop().catch(error => console.log(error));
+    }
+  }
+}
