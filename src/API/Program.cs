05d4d25ba4764199742d5b323fd8b6f9e7a96240
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-06-03 18:28:58 -0600

    Parcial06 - SignalR - Add presence hub security

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -102,6 +102,21 @@ public static class Program
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+                        
+                        return Task.CompletedTask;
+                    }
+                };
             });
         builder.Services.AddAuthorizationBuilder()
             .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
