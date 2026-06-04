b8210afc832ef6b703927c3172acff20e5b162fe
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-06-03 18:08:55 -0600

    Parcial06 - IDENTITY - Add role edition

--- src/API/Controllers/AdminController.cs
+++ src/API/Controllers/AdminController.cs
@@ -29,6 +29,29 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
+    [HttpPut("roles/{userId}")]
+    public async Task<ActionResult> EditRoles(string userId, [FromQuery] string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("You must provide at least one role");
+
+        var selectedRoles = roles.Split(",").ToArray();
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (user == null) return BadRequest("Could not retrieve user");
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to add roles");
+
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Failed to remove roles");
+
+        return Ok(await userManager.GetRolesAsync(user));
+    }
+
+    [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
     public ActionResult GetPhotosForModeration()
     {
